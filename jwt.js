29c9cc043bc50110c0c1ad57e10922219093f89e
Will Carpenter <willcarpenteriii@gmail.com>
2017-09-03 12:27:31 -0600

    added jwt encryption

---
+++ jwt.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken')
+
+const data = [{
+	id:       1,
+	password: 'abc1234'
+}, {
+	id:       2,
+	password: 'password!'
+}, {
+	id:       3,
+	password: 'suckitTrebek420'
+}]
+
+const salt = 'secret_sauce'
+
+//  Encrypted
+const strData = JSON.stringify(data)
+
+const token = jwt.sign( strData, salt )
+
+console.log(`The jwt is: ${token}`)
+
+//  Decrypted
+const decrypted = jwt.verify(token, salt)
+
+const strDecrypt = JSON.stringify(decrypted)
+
+
+console.log(`Decrypted is ${ strDecrypt }`)
